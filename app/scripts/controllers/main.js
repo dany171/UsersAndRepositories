618e93c6a5c8942e0753f30466a246d27aaf9631
daniel e <dany171@gmail.com>
2017-04-04 23:06:35 -0400

    generating project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name urApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the urApp
+ */
+angular.module('urApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
